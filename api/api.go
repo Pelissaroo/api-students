0ca66ce756ad248a2aa012f4c0aa2d68b0dfc027
unknown <vitorpelissaro01@gmail.com>
2025-06-05 11:35:44 -0300

    create handler to get student by id

--- api/api.go
+++ api/api.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/Pelissaroo/api-students/db"
 
 	"github.com/labstack/echo/v4"
@@ -41,43 +38,4 @@ func (api *API) ConfigureRoutes() {
 	api.Echo.GET("/students/:id", api.getStudent)
 	api.Echo.PUT("/students/:id", api.updateStudent)
 	api.Echo.DELETE("students/:id", api.deleteStudent)
-}
-
-func (api *API) getStudents(c echo.Context) error {
-	students, err := api.DB.GetStudents()
-	if err != nil {
-		return c.String(http.StatusNotFound, "Failed to get students ")
-	}
-
-	return c.JSON(http.StatusOK, students)
-}
-
-func (api *API) createStudent(c echo.Context) error {
-	student := db.Student{}
-	if err := c.Bind(&student); err != nil {
-		return err
-	}
-	if err := api.DB.AddStudent(student); err != nil {
-		return c.String(http.StatusOK, "Create student")
-	}
-
-	return c.String(http.StatusInternalServerError, "Error to create student")
-}
-
-func (api *API) getStudent(c echo.Context) error {
-	id := c.Param("id")
-	getStud := fmt.Sprintf("Get %s Student", id)
-	return c.String(http.StatusOK, getStud)
-}
-
-func (api *API) updateStudent(c echo.Context) error {
-	id := c.Param("id")
-	updateStud := fmt.Sprintf("Update %s Student", id)
-	return c.String(http.StatusOK, updateStud)
-}
-
-func (api *API) deleteStudent(c echo.Context) error {
-	id := c.Param("id")
-	deleteStud := fmt.Sprintf("Delete %s Student", id)
-	return c.String(http.StatusOK, deleteStud)
 }
